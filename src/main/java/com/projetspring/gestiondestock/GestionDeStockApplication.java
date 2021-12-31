ce3ae5838c37a2f5dbe231392dcb28f838a71763
Kadir-King <kadastkev@gmail.com>
2021-12-31 17:17:38 +0100

    premier projet spring

--- src/main/java/com/projetspring/gestiondestock/GestionDeStockApplication.java
+++ src/main/java/com/projetspring/gestiondestock/GestionDeStockApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestionDeStockApplication {
     public static void main(String[] args) {
         SpringApplication.run(GestionDeStockApplication.class, args);
+        System.out.println("Hello world !");
     }
 
 
